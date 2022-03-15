099294e3539856dce49883b171aed64fbf3acb38
Samas1503 <samueleliasparedes.10@gmail.com>
2022-03-15 01:17:40 -0300

    Crud final

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+//parse aplication/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse aplication/json
+app.use(bodyParser.json());
 
 require('dotenv').config()
 
--- router/mascotas.js
+++ router/mascotas.js
@@ -16,4 +16,82 @@ router.get('/', async(req, res) => {
 
 })
 
+router.get('/crear', (req, res) => {
+    try {
+        res.render('crear')
+    } catch (e) {
+        console.log(e);
+    }
+})
+
+router.post('/', async(req, res) => {
+    const body = req.body;
+    console.log(body);
+    try {
+        // const mascotaDB = new Mascota(body);
+        // await mascotaDB.save();
+
+        await Mascota.create(body);
+
+        res.redirect('/mascotas')
+    } catch (e) {
+        console.log(e);
+    }
+})
+
+router.get('/:id', async(req, res) => {
+    const id = req.params.id;
+    try {
+        const mascotaDB = await Mascota.findOne({ _id: id });
+        res.render('detalle', {
+            mascota: mascotaDB,
+            error: false
+        })
+    } catch (e) {
+        console.log(e);
+        res.render('detalle', {
+            error: true,
+            mensaje: 'No se encuentra el ID de la mascota'
+        })
+    }
+})
+
+router.delete('/:id', async(req, res) => {
+    const id = req.params.id;
+    try {
+        const mascotaDB = await Mascota.findByIdAndDelete({ _id: id });
+        if (mascotaDB) {
+            res.json({
+                estado: true,
+                mensaje: 'Eliminado'
+            })
+        } else {
+            res.json({
+                estado: false,
+                mensaje: 'No se puede eliminar'
+            })
+        }
+    } catch (e) {
+        console.log(e);
+    }
+})
+
+router.put('/:id', async(req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    try {
+        const mascotaDB = await Mascota.findByIdAndUpdate(id, body, { useFindAndModify: false });
+        res.json({
+            estado: true,
+            mesaje: 'Editado'
+        })
+    } catch (e) {
+        console.log(e);
+        res.json({
+            estado: false,
+            mesaje: 'Fallo Editar'
+        })
+    }
+})
+
 module.exports = router;
